134c9fd29abb823084a87536921eb2d46f63b786
mmahnoosh <montazer_mahnoosh@gmx.de>
2025-06-06 16:28:45 +0200

    add the functions: - add_post - delete_post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,12 +7,45 @@ CORS(app)  # This will enable CORS for all routes
 POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
+    {"id": 3, "title": "Second post", "content": "This is the second post."},
 ]
 
+
+# app.secret_key = os.environ.get("secret_key")
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    data = request.get_json()
+    new_title = data.get('title', '').strip()
+    new_content = data.get('content', '').strip()
+
+    if not new_title or not new_content:
+        return jsonify({"error": "All fields must be filled in."}), 400
+
+    new_id = max([post["id"] for post in POSTS], default=0) + 1
+    new_post = {
+        "id": new_id,
+        "title": new_title,
+        "content": new_content
+    }
+    POSTS.append(new_post)
+    return jsonify(new_post), 201
+
+
+@app.route('/api/posts/<id>', methods=['DELETE'])
+def delete_post():
+    for post in POSTS:
+        if str(post['id']) == str(id):
+            POSTS.remove(post)
+        print(f"id:, {id}, Post: {post}, {type(id)}, {type(post['id'])}")
+
+    # POSTS = new_list
+    return {"message": "Post with id <id> has been deleted successfully."}, 200
 
 
 if __name__ == '__main__':
--- frontend/frontend_app.py
+++ frontend/frontend_app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def home():
